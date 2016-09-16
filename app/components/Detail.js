06e22b84caade25007df2307a4c664d173c3e331
stevenjmarsh <sjm.atl@gmail.com>
2016-09-16 18:09:10 -0400

    adding formating / style of day detail weather

--- app/components/Detail.js
+++ app/components/Detail.js
@@ -3,10 +3,29 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
+var utils = require("../utils/utils");
+var convertTemp = utils.convertTemp;
+
+var styles = {
+  descriptionContainer: {
+    fontSize: 34,
+    fontWeight: 100,
+    maxWidth: 400,
+    margin: '0 auto',
+    textAlign: 'center',
+  }
+};
+
 function Detail(props) {
   console.log("props in detail:", props);
   return (
-    <div>Detail for {props.city}'s weather.</div>
+    <div style={styles.descriptionContainer}>
+      <p>{props.city}</p>
+      <p>{props.weather.weather[0].description}</p>
+      <p>min temp: {convertTemp(props.weather.temp.min)} degrees</p>
+      <p>max temp: {convertTemp(props.weather.temp.max)} degrees</p>
+      <p>humidity: {props.weather.humidity}</p>
+    </div>
   );
 }
 
